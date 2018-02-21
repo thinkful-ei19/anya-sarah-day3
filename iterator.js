5c9df747ce5f55c0b69167598323e4584887eed5
Anya <anya.p.nguyen@gmail.com>
2018-02-21 11:16:59 -0800

    added iterator.js

---
+++ iterator.js
@@ -0,0 +1,18 @@
+/*
+Create an object with five properties: foo, bar, fum, quux, and spam. Give each property a unique value of your choosing.
+Loop over the object using for ... in
+Use console.log to show each property name and its associated value.
+*/
+
+const obj = {
+    foo: 'foo',
+    bar: 'bar',
+    fum: 'fum',
+    quux: 'quux',
+    spam: 'spam'
+}
+
+for (let key in obj) {
+    const value = obj[key]; 
+    console.log(value);
+}
